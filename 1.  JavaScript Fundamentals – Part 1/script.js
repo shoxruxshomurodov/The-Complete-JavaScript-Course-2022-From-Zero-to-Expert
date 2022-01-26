b485f2a5f41aa6c0f9bd019709f573a6105aaa4d
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2022-01-26 16:42:44 +0500

    add:new topics

---
+++ 1.  JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,49 @@
+//HELLO WORLD
+/*
+alert("Hello Shoxrux!");
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+js = "boring";
+("boring");
+if (js === "amazing") alert("JavaScript is FUN!");
+25 + 13 - 10;
+ */
+
+//A Brief Introduction to JavaScript
+/*
+---------------------------------
+WHAT IS JAVASCRIPT ?
+
+JavaScript is a High-level,
+Object-Oriented , Multi-Paradigm
+programming language.
+---------------------------------
+
+_____________ECMA Script Versions______________
+ES5 -> ES6/ES2015 -> ES7/ES2016 -> ES8/ES2017 -> ES9/ES2018 -> ES10/ES2019 -> ES11/ES2020 -> ...
+
+ES5 -> ECMAScript
+ES6/ES2015 -> Biggest update to the language EVER✨
+
+//Linking a JavaScript File
+
+Internal->>>>>>
+<script>
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+</script>
+
+External->>>>>>
+<script src="script.js"> </script>
+*/
+
+//Values and Variables
+let js = "amazing";
+console.log(65 - 32 + 10);
+console.log("Shoxrux");
+console.log(23);
+
+let firstName = "Muhtarama";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
