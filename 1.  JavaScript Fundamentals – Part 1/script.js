28b99c481ed3d52cfe6046b0e6e2023b5356c1a1
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2022-01-27 12:38:09 +0500

    Data:types

--- 1.  JavaScript Fundamentals – Part 1/script.js
+++ 1.  JavaScript Fundamentals – Part 1/script.js
@@ -35,7 +35,6 @@ if (js === "amazing") alert("JavaScript is FUN!");
 
 External->>>>>>
 <script src="script.js"> </script>
-*/
 
 //Values and Variables
 let js = "amazing";
@@ -44,6 +43,50 @@ console.log("Shoxrux");
 console.log(23);
 
 let firstName = "Muhtarama";
+
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+//not allowed naming
+// let 3years = 3;
+// let shoxrux&shomurodov = "ShSh"
+let shoxrux_shomurodov = "ShSh";
+let shoxrux$shomurodov = "ShSh";
+//Not allowed reserved word
+// let new = "new year"
+
+//Reserved but allowed
+let name = "Muhtarama"; //Not Reccomended
+
+//Not begin with Uppercase just convention
+let Person = "Shoxrux"; //for Objects
+let PI = 3.14;
+console.log(PI);
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+//Better than above
+let myFirstJob = "Programmer";
+let myCurrnetJob = "Teacher";
+*/
+
+//-----Data Types-----//
+
+// 7 Primitive Data types
+/*
+1-> Number = Floating Points Number 👉 used for decimal and integers
+2-> String = Sequence of characters 👉 used for text
+3-> Boolean = Logical type that can only be "true" or "false" 👉 used for taking decision
+4-> Undefined = Value taken by a variable that is not yet defined ("empty value")
+5-> Null = also means "empty value"
+6-> Symbol(ES2015/ES6) = value that is unique and cannot be changed 
+7-> BigInt(ES2020/ES11) = Larger integers than the Number type can hold
+*/
+/*
+👆JavaScript has dynamic typing : We do not have to manually define 
+the data types of the value stored in a variable. Instead, data types are 
+determined automatically
+ */
+
+//Objects (Reference) Data types
