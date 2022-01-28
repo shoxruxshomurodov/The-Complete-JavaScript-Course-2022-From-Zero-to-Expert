28c492607912dd014080f6af6de34c8056ba34b5
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2022-01-28 18:07:04 +0500

    complete 1st section

--- 1.  JavaScript Fundamentals – Part 1/script.js
+++ 1.  JavaScript Fundamentals – Part 1/script.js
@@ -573,4 +573,38 @@ console.log(`I am ${2022 - 1998} years old.`);
  2009 ES5(ECMAScript5)
  2015 ES6 - biggest update to the language ever!
  2016 - ∞∞∞
+
+ Backwards Compatilibility : Don't break the web!
+
+Modern JavaScript Engine---------------> 1997 ES1
+☝Old features are never removed
+☝Not really new versions , just incremental updates (releases)
+☝Websites keep working forever!
+
+
+☝BABEL
  */
+
+/*
+Codding Challenge #4🔥
+
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. 
+In his country, it's usual to tip 15% if the bill value is between 50 and 300.
+ If the value is different, the tip is 20%.
+1. Your task is to caluclate the tip, depending on the bill value.
+ Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅
+  (If it's easier for you, you can start with an if/else statement, 
+  and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip,
+ and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25,
+  and the total value 316.25'
+TEST DATA: Test for bill values 275, 40 and 430
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+*/
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+const total = bill + tip;
+
+console.log(`Bill is ${bill} , tip is ${tip} . Total = ${total}`);
