a9aba7924ccc742dabfe912a026e4a307b68bf80
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2022-02-04 10:39:50 +0500

    object:challenge:complete

--- 2.  JavaScript Fundamentals – Part 2/script.js
+++ 2.  JavaScript Fundamentals – Part 2/script.js
@@ -329,6 +329,7 @@ console.log(
 
 //**---Objects Methods---**//
 
+/*
 const shoxrux = {
   firstName: "Shoxrux",
   lastName: "Shomurodov",
@@ -382,3 +383,50 @@ console.log(
 );
 
 console.log(shoxrux.getSummary());
+ */
+
+// Coding Challenge #3🔥
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+GOOD LUCK 😀
+*/
+
+const shoxrux = {
+  fullName: "Shoxrux Shomurodov",
+  height: 1.69,
+  weight: 78,
+  calcBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const shahboz = {
+  fullName: "Shahboz Rasulov",
+  height: 1.95,
+  weight: 92,
+  calcBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+shahboz.calcBMI();
+console.log(shahboz.bmi);
+shoxrux.calcBMI();
+console.log(shoxrux.bmi);
+
+if (shoxrux.bmi > shahboz.bmi) {
+  console.log(
+    `${shoxrux.fullName}'s BMI:(${shoxrux.bmi}) is hiegher than ${shahboz.fullName}'s BMI (${shahboz.bmi}) `
+  );
+} else if (shoxrux.bmi < shahboz.bmi) {
+  console.log(
+    `${shahboz.fullName}'s BMI:(${shahboz.bmi}) is hiegher than ${shoxrux.fullName}'s BMI (${shoxrux.bmi}) `
+  );
+}
