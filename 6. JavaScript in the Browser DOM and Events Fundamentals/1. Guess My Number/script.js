6b7ce10ad50a8b0f34585f0571ccb65b4e494bb5
Shoxrux <shomurodovshoxrux1998@gmail.com>
2022-02-15 12:23:10 +0500

    add:new section

--- 6. JavaScript in the Browser DOM and Events Fundamentals/1. Guess My Number/script.js
+++ 6. JavaScript in the Browser DOM and Events Fundamentals/1. Guess My Number/script.js
@@ -1,3 +1,10 @@
+/*
+-------What is DOM ?--------
+
+DOM (DOCUMENT OBJECT MODEL ) : Structured representation of html documents ,
+allows JavaScript to access HTML elements and styles to manipulate them.
+*/
+
 "use strict";
 
 const message = document.querySelector(".message").textContent;
