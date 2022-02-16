8e715962d2f10a37c543161cf5e48f18d0be24fe
Shoxrux <shomurodovshoxrux1998@gmail.com>
2022-02-16 12:37:19 +0500

    fix

--- 6. JavaScript in the Browser DOM and Events Fundamentals/3. Pig Game/script.js
+++ 6. JavaScript in the Browser DOM and Events Fundamentals/3. Pig Game/script.js
@@ -69,7 +69,7 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     //2. Check if player's  score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       //Finish Game
       playing = false;
       diceEl.classList.add("hidden");
