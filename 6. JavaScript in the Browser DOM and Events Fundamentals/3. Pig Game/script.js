5bda26e5a25d94039e584832edb13a1dd175b9fa
Shoxrux <shomurodovshoxrux1998@gmail.com>
2022-02-16 12:34:15 +0500

    piggame:project

---
+++ 6. JavaScript in the Browser DOM and Events Fundamentals/3. Pig Game/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+//Selecting Elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+//Starting Condition
+let scores, currentScore, playing, activePlayer;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+//Rolling Dice functionality
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `./images/dice-${dice}.png`;
+
+    //3. Checked for rolled 1
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch to  switch player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //2. Check if player's  score is >= 100
+    if (scores[activePlayer] >= 10) {
+      //Finish Game
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+//Reset Game
+
+btnNew.addEventListener("click", init);
