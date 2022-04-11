76d6b71d504abd9c0c194658fc99f5e20e5aec16
Shoxrux <shomurodovshoxrux1998@gmail.com>
2022-04-11 17:32:31 +0500

    add:FinalProject

--- 9. Functions/script.js
+++ 9. Functions/script.js
@@ -391,19 +391,16 @@ booker();
 console.dir(booker);
 
 // Coding Challenge #2 🔥🔥🔥
-
 /* 
-
 This is more of a thinking challenge than a coding challenge 🤓
 Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
 And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
 GOOD LUCK 😀
-
 */
 
-const header = document.querySelector("h1");
-const changeColor = () => {
-  header.style.color = "blue";
-};
+// const header = document.querySelector("h1");
+// const changeColor = () => {
+//   header.style.color = "blue";
+// };
 
-document.querySelector("body").addEventListener("click", changeColor);
+// document.querySelector("body").addEventListener("click", changeColor);
